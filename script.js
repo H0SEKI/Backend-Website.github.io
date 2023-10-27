9c7f08f1f5afc874238bc22f0b7f57f332ab016d
GideonRian <edysdeon@gmail.com>
2023-10-27 18:30:23 +0700

    uii

--- script.js
+++ script.js
@@ -17,19 +17,16 @@ fetch('dictionary.json')
     console.error('Terjadi kesalahan:', error);
   });
 
-  fetch('dictionary.json') // Mengambil data dari file JSON
+  fetch('dictionary.json')
   .then(response => response.json())
   .then(data => {
-    // Loop melalui data API
+    
     data.forEach(item => {
-      // Ambil judul buku dan URL gambar
       const judul = item.Judul;
       const gambarUrl = item.Image;
 
-      // Cari elemen HTML berdasarkan judul buku
       const gambarElement = document.getElementById(`gambar-${judul}`);
 
-      // Jika elemen gambar ditemukan, atur atribut src dengan URL gambar
       if (gambarElement) {
         gambarElement.src = gambarUrl;
       }
